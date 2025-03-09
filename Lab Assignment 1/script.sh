ea2d49fdcd39926b91b7d643e5aad5b181e8f0b9
shelby2770 <ajm.istiaque@g.bracu.ac.bd>
2025-03-09 16:15:47 +0600

    first assignment done

--- Lab Assignment 1/script.sh
+++ Lab Assignment 1/script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-yacc -d -y --debug --verbose syntax_analyzer.y
+win_bison -d -y --debug --verbose 22101451.y
 echo 'Generated the parser C file as well the header file'
 g++ -w -c -o y.o y.tab.c
 echo 'Generated the parser object file'
-flex lex_analyzer.l
+win_flex 22101451.l
 echo 'Generated the scanner C file'
 g++ -fpermissive -w -c -o l.o lex.yy.c
-# if the above command doesn't work try g++ -fpermissive -w -c -o l.o lex.yy.c
 echo 'Generated the scanner object file'
 g++ y.o l.o
 echo 'All ready, running'
