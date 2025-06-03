67e1c680cc019e954671c21ee1ec6b66298e376c
shelby70 <ajm.istiaque@g.bracu.ac.bd>
2025-06-03 20:00:04 +0600

    assignment 2 updated

---
+++ Lab Assignment 2/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+win_bison -d -y --debug --verbose 23341065.y
+echo 'Generated the parser C file as well the header file'
+g++ -w -c -o y.o y.tab.c
+echo 'Generated the parser object file'
+win_flex 23341065.l
+echo 'Generated the scanner C file'
+g++ -fpermissive -w -c -o l.o lex.yy.c
+# if the above command doesn't work try g++ -fpermissive -w -c -o l.o lex.yy.c
+echo 'Generated the scanner object file'
+g++ y.o l.o
+echo 'All ready, running'
+./a.exe input.c
+echo 'logfile'
+cat my_log.txt
